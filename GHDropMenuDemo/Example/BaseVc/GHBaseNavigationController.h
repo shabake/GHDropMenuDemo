8e6e5f2a0120b59c8f7d60da6bf7786ac72d7e71
shabake <45329453@qq.com>
2019-01-01 00:41:23 +0800

    增加导航控制器基类

---
+++ GHDropMenuDemo/Example/BaseVc/GHBaseNavigationController.h
@@ -0,0 +1,17 @@
+//
+//  GHBaseNavigationController.h
+//  GHDropMenuDemo
+//
+//  Created by zhaozhiwei on 2019/1/1.
+//  Copyright © 2019年 GHome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GHBaseNavigationController : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GHDropMenuDemo/Example/BaseVc/GHBaseNavigationController.m
@@ -0,0 +1,22 @@
+//
+//  GHBaseNavigationController.m
+//  GHDropMenuDemo
+//
+//  Created by zhaozhiwei on 2019/1/1.
+//  Copyright © 2019年 GHome. All rights reserved.
+//
+
+#import "GHBaseNavigationController.h"
+
+@interface GHBaseNavigationController ()
+
+@end
+
+@implementation GHBaseNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
